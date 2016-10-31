096e4a21e74c57b8d88c66036496a89a304bc783
Matthew Clayton <claytomp@mail.uc.edu>
2016-10-31 19:48:45 -0400

    Initial commit of Part 2, Task 1

---
+++ ohms1.cpp
@@ -0,0 +1,18 @@
+/*
+* @author Matthew Clayton
+* @date 31 October 2016
+* @assign.ment Lab 8 Ohm's Law
+* @descrip.tion
+* This file contains code for Task 1 of Part of L8. This program
+* allows for 'unlimited' resistors by using the vector class. The
+* program outputs total power, series current, voltage source, and
+* power and resistance for each resistor. Task 1 implements a
+* basic simple 1-node DC CKT Simulator.
+*/
+
+#include <iostream>
+using namespace std;
+
+class Ohms1 {
+  
+}
